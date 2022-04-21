e01b5b8086e25ead3331c7dd12abdf6acfd7e96b
deepika tejani <dpkatejani@gmail.com>
2022-04-21 12:09:34 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mvvmdemo/QuoteRepository.kt
@@ -0,0 +1,12 @@
+package com.example.mvvmdemo
+
+import androidx.lifecycle.LiveData
+
+class QuoteRepository(private val quoteDao: QuoteDao){
+    fun getQuotes(): LiveData<List<Quote>>{
+        return quoteDao.getQuotes()
+    }
+    suspend fun insertQuotes(quote: Quote){
+        quoteDao.insertQuote(quote)
+    }
+}
